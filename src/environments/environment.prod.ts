b4d1c51c0329b359e68925d6481c27b8c208e8b5
Your Name <you@example.com>
2022-01-05 22:57:33 +0700

    add

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  //API_LOCAL
+  API_LOCAL: 'http://localhost:8080/api/',
+  //API_SERVE
+  API_SERVE: 'https://banhang2022.herokuapp.com/api/',
+  production: true
+};
